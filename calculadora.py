918108359280b7ec3e836a13ccf0fe9a2ea7395c
Sebadalpra <sebastiandalpra@gmail.com>
2024-05-24 00:32:55 -0300

    Creacion repositorio

---
+++ calculadora.py
@@ -0,0 +1,100 @@
+from pack.funciones import *
+from os import *
+
+"""
+a- Ingreso un operando
+b- Ingreso segundo operando
+c- Operaciones
+d- Mostrar Resultado
+e- Salir
+
+Se van desbloqueando cuando el anterior se realizo
+Una ver Mostrado el resultado se bloquea todo de nuevo
+Lo unico no bloqueado es el punto a y e al inicio
+Cuidado con el erro de dividir por 0
+"""
+
+def limpiar_pantalla():
+    system("cls")
+
+def menu():
+    print(f"""
+    CALCULADORA
+a- Ingresar primer valor
+b- Ingresar segundo valor
+c- Operaciones
+d- Mostrar Resultado
+e- Salir
+                    
+x = {operando_a}         
+y = {operando_b}         
+""")
+    return input("\nSeleccione una opcion del menu: \n") # esta dentro de funcion menu
+
+def submenu():
+    print(f"""
+1- Sumar
+2- Restar
+3- Dividir
+4- Multiplicar
+5- Factorial
+""")
+
+primer_paso = False
+segundo_paso = False
+tercer_paso = False
+operando_a = None
+operando_b = None
+
+def validar_e_ingresar_numero()-> int:
+    while True:
+        try:
+            numero = int(input("Ingrese un número: "))
+            return numero
+        except ValueError:
+            print("ERROR: Ingrese un número válido.")
+
+while True:
+    match menu():
+        case "a":
+            numero_uno = validar_e_ingresar_numero()
+            primer_paso = True
+            operando_a = numero_uno
+            limpiar_pantalla()
+        case "b":
+            if primer_paso:
+                numero_dos = validar_e_ingresar_numero()
+                segundo_paso = True
+                operando_b = numero_dos
+                limpiar_pantalla()
+            else:
+                print("\nDebe ingresar el primer valor\n")
+                system("pause")
+                limpiar_pantalla()
+        case "c":
+            if segundo_paso:
+                submenu()
+                resultado = whileOpciones(numero_uno,numero_dos)
+                limpiar_pantalla()
+                tercer_paso = True
+            else:
+                print("\nDebe ingresar un valor primero\n")
+                system("pause")
+                limpiar_pantalla()
+        case "d":
+            if tercer_paso:
+                print(f"El resultado es: {resultado}")
+                system("pause")
+                limpiar_pantalla()
+            elif segundo_paso:
+                print("\nNo podes ver el resultado sin ingresar un operador\n")
+                system("pause")
+                limpiar_pantalla()
+            else:
+                print("\nDebe ingresar un valor primero\n")
+                system("pause")
+                limpiar_pantalla()
+        case "e":
+            break
+
+
---
+++ pack/funciones.py
@@ -0,0 +1,47 @@
+from os import *
+
+def suma(valor_uno: int, valor_dos: int)-> int:
+    return valor_uno + valor_dos
+
+def resta(valor_uno: int, valor_dos: int)-> int:
+    return valor_uno - valor_dos
+
+def dividir(valor_uno: int, valor_dos: int)-> float:
+    return valor_uno / valor_dos
+
+def multiplicar(valor_uno: int, valor_dos: int)-> int:
+    return valor_uno * valor_dos
+
+def factorial(valor_uno: int) -> int:
+    if valor_uno < 0:
+        print("No se puede calcular el factorial de un número negativo.")
+        return None
+    elif valor_uno == 0 or valor_uno == 1:
+        return 1
+    else:
+        fact = 1
+        for i in range(2, valor_uno + 1):
+            fact *= i
+        return fact
+
+def whileOpciones(valor_uno: int, valor_dos: int):
+    while True:
+        opcion = input("\nIngrese un opcion : \n")
+        match opcion.lower():
+            case "sumar" | "1":
+                return suma(valor_uno,valor_dos)
+            case "restar" | "2":
+                return resta(valor_uno,valor_dos)
+            case "dividir" | "3":
+                try:
+                    return dividir(valor_uno,valor_dos)
+                except ZeroDivisionError:
+                    print("No se puede dividir por cero")
+                    system("pause")
+                    return
+            case "multiplicar" | "4":
+                return multiplicar(valor_uno,valor_dos)
+            case "factorial" | "5":
+                return factorial(valor_uno)
+
+            
